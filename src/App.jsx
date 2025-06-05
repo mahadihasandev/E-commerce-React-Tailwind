fa67172ac8e25967c05d55f15d5f219f887c517c
mahadihasandev <mayhaydihasan.com@gmail.com>
2025-06-05 10:35:03 +0600

    router tailwind vite setup

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+    <Route path="/" element={<Home />}></Route>
+    <Route path="/shop" element={<Shop />}></Route>
+    </>
+  )
+);
+
+function App() {
+  return (
+    <>
+    <RouterProvider router={router} />
+    </>
+  )
+}
+
+export default App
