8e1d46a84d252f7f5b1f43efa0875943a7947098
Mahadi Hasan Arnob <mayhaydihasan.com@gmail.com>
2025-06-12 22:17:57 +0600

    Navbar update

--- src/component/List.jsx
+++ src/component/List.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function List({text,className}) {
   return (
     <>
-        <li className={`font-DMs w-12 h-5 text-7 font-normal text-lightDark hover:text-deepDark hover:font-bold ${className}`}>{text}</li>
+        <li className={`font-DMs w-12 h-5 text-7 font-normal text-lightDark hover:text-deepDark hover:font-bold duration-300 ${className}`}>{text}</li>
     </>
   )
 }
