5e5982b3224cd5e1ed889247df4237b0a79ae5af
Mahadi Hasan Arnob <mayhaydihasan.com@gmail.com>
2025-06-23 10:15:51 +0600

    all page making

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,28 @@ import {
 } from "react-router-dom";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
+import ProductDetails from "./pages/ProductDetails";
+import About from "./pages/About";
+import Contacts from "./pages/Contacts";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Error from "./pages/Error";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
     <Route path="/" element={<Home />}></Route>
     <Route path="/shop" element={<Shop />}></Route>
+    <Route path="/products" element={<ProductDetails />}></Route>
+    <Route path="/about" element={<About/>}></Route>
+    <Route path="/contacts" element={<Contacts/>}></Route>
+    <Route path="/login" element={<Login/>}></Route>
+    <Route path="/signup" element={<Signup/>}></Route>
+    <Route path="/cart" element={<Cart/>}></Route>
+    <Route path="/checkout" element={<Checkout/>}></Route>
+    <Route path="/error" element={<Error/>}></Route>
     </>
   )
 );
