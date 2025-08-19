6b204a2b2131a267def3d282965760f5a2b5cfdc
mahadihasandev <mayhaydihasan.com@gmail.com>
2025-08-19 09:23:01 +0600

    mock api for pagination

---
+++ src/data.js
@@ -0,0 +1,65 @@
+import product from './assets/cardImg1.jpg'
+const data=[
+    {
+        id: 1,
+        title: "Product 1",
+        price: 29.99,
+        image: {product},
+    },
+    {
+        id: 2,
+        title: "Product 2",
+        price: 39.99,
+        image: {product},
+        
+    },
+    {
+        id: 3,
+        title: "Product 3",
+        price: 49.99,
+        image: {product}
+    },
+    {
+        id: 4,
+        title: "Product 4",
+        price: 59.99,
+        image: {product}
+    },
+    {
+        id: 5,
+        title: "Product 5",
+        price: 69.99,
+        image: {product}
+    },
+    {
+        id: 6,
+        title: "Product 6",
+        price: 79.99,
+        image: {product}
+    },
+    {
+        id: 7,
+        title: "Product 7",
+        price: 89.99,
+        image: {product}
+    },
+    {
+        id: 8,
+        title: "Product 8",
+        price: 99.99,
+        image: {product}
+    },
+    {
+        id: 9,
+        title: "Product 9",
+        price: 109.99,
+        image: {product}
+    },
+    {
+        id: 10,
+        title: "Product 10",
+        price: 119.99,
+        image: {product}
+    }
+
+]
